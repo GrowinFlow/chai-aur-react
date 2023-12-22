eac88719953f9b4224d4a6e1c37d5ff4fc3dcefa
John Doe <growinflowofficial@gmail.com>
2023-12-22 04:22:52 -0800

    delete files and changings

---
+++ 01vitereact/src/Card.jsx
@@ -0,0 +1,18 @@
+function Card(){
+    return(
+        <>
+        <p>
+        <h2>About</h2>
+        <pre>
+            Lorem ipsum, dolor sit amet
+            consectetur adipisicing 
+            elit.              Hic, quos quo omnis ullam
+            facere modi distinctio 
+            iure ratione ut explicabo!
+        </pre>
+        </p>
+        </>
+    )
+}
+
+export default Card
