2c321f828f253b3f4dc7a1fd439426a3f04570ea
John Doe <growinflowofficial@gmail.com>
2023-12-21 22:08:10 -0800

    app.jsx in chaning

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,11 +1,8 @@
-import { useState } from 'react'
-
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
    <h1>
-    Hy! This is first app
+    Hy! This is first app || vite + react
    </h1>
   )
 }
